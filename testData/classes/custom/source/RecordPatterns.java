c524dd441d606f0f2fd6723a0a131aabcf3c47ac
Luna <izzeldeen03@gmail.com>
2022-06-18 13:23:38 +0100

    more complex test

--- testData/classes/custom/source/RecordPatterns.java
+++ testData/classes/custom/source/RecordPatterns.java
@@ -1,8 +1,10 @@
-
+import java.util.Arrays;
 
 public class RecordPatterns{
 	
 	record Point(int x, int y){}
+	record Point3(int x, int y, int z){}
+	record S<A, B, Xs>(A a, B b, Xs... xs){}
 	enum Colour{ RED, GREEN, BLUE }
 	
 	sealed interface Shape{}
@@ -25,9 +27,33 @@ public class RecordPatterns{
 			case Circle c -> "null circle lol";
 			case null -> "null2";
 		});
+		
+		var p3 = fromTextP(args);
+		
+		if(p3 instanceof Point3(int x, int y, int z)){
+			System.out.println(x + y + z);
+		}
+		
+		if(p3 instanceof Point3(int a, int b, int c) d && a > 0 && b == 2){
+			System.out.println(c);
+		}
+		
+		var s = fromTextS(args);
+		
+		if(s instanceof S<?,?,?>(Integer i, String st, Object[] os)){
+			System.out.println("an S of " + i + " and " + st + " and " + Arrays.toString(os));
+		}
 	}
 	
 	public static Shape fromText(String[] s){
+		return null;
+	}
+	
+	public static Point3 fromTextP(String[] s){
+		return null;
+	}
+	
+	public static S fromTextS(String[] s){
 		return null;
 	}
 }
